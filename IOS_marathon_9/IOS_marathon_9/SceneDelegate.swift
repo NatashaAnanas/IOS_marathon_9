f8ce58cfb6f4aeb51dc8ffae1965bcee0da393cb
NatashaAnanas <konnat7@mail.ru>
2024-02-26 11:29:59 +0300

    Base settings

---
+++ IOS_marathon_9/IOS_marathon_9/SceneDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  SceneDelegate.swift
+//  IOS_marathon_9
+//
+//  Created by Наталья Коновалова on 26.02.2024.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
---
+++ IOS_marathon_9/IOS_marathon_9/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  IOS_marathon_9
+//
+//  Created by Наталья Коновалова on 26.02.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
